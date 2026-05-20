1ae49f2debe5f6308d2f356528ec78ad329ed631
Lucas Fabiano <lucasfafa6@gmail.com-at-851479049298>
2026-05-20 14:18:22 -0300

    INVAI-8 - Configuração de Spring Security e filtro JWT

---
+++ src/main/java/com/investai/api/module/auth/service/UsuarioDetailsService.java
@@ -0,0 +1,21 @@
+package com.investai.api.module.auth.service;
+
+import com.investai.api.module.auth.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+
+}
